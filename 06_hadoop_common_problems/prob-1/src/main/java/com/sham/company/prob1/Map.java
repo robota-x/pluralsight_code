8c9a0653cf05a77f115859f01b21773a91774d2d
Lorenzo Turrino <lorenzo.turrino@bbc.co.uk>
2018-01-12 22:59:46 +0000

    add mapper class

--- 06_hadoop_common_problems/prob-1/src/main/java/com/sham/company/prob1/Map.java
+++ 06_hadoop_common_problems/prob-1/src/main/java/com/sham/company/prob1/Map.java
@@ -1,21 +1,31 @@
 package com.sham.company.prob1;
 
-import org.apache.hadoop.io.IntWritable;
+
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
 
-public  class Map {
+public class Map extends Mapper<LongWritable, Text, Text, Text> {
+
+    @Override
+    public void map(LongWritable key, Text value, Context context)
+            throws IOException, InterruptedException {
+        String line = value.toString();
+        String[] dataList = line.split(",");
+
+        try {
+            String age = dataList[0];
+            String occupation = dataList[8];
+            Double weight = Double.parseDouble(dataList[24]);
+
+            // ugly stopgap
+            for (int i=0; i<=weight; i++) {
+                context.write(new Text(age), new Text(occupation));
+            }
+        } catch (Exception e) {
+            // pass
+        }
+    }
 }
-
-
-
-
-
-
-
-
-
-
