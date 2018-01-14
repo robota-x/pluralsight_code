66bec638d13243c6f02bf6f1717d7e2a7ec3ee75
Lorenzo Turrino <lorenzo.turrino@bbc.co.uk>
2018-01-14 09:59:29 +0000

    add reducer class

--- 06_hadoop_common_problems/prob-1/src/main/java/com/sham/company/prob1/Reduce.java
+++ 06_hadoop_common_problems/prob-1/src/main/java/com/sham/company/prob1/Reduce.java
@@ -1,13 +1,31 @@
 package com.sham.company.prob1;
 
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
 
-public class Reduce {
+public class Reduce extends Reducer<Text, Text, Text, Text> {
+
+    @Override
+    public void reduce(final Text key,
+                       final Iterable<Text> values,
+                       final Context context)
+            throws IOException, InterruptedException {
+        HashMap<String, Integer> occupationFrequency = new HashMap<>();
+
+        for (Text occupation : values) {
+            String occ = occupation.toString();
+            Integer count = occupationFrequency.getOrDefault(occ, 0);
+
+            occupationFrequency.put(occ, count + 1);
+        }
+
+        String mostCommonOccupation = Collections.max(occupationFrequency.entrySet(), HashMap.Entry.comparingByValue()).getKey();
+        context.write(key, new Text(mostCommonOccupation));
+    }
 
 }
 
