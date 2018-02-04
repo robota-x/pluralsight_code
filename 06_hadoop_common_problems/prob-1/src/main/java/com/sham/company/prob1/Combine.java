aee9f3bab0cc22cfcb975219483cbfe2bd18575e
Lorenzo Turrino <lorenzo.turrino@bbc.co.uk>
2018-02-04 11:29:59 +0000

    add WIP combiner class

---
+++ 06_hadoop_common_problems/prob-1/src/main/java/com/sham/company/prob1/Combine.java
@@ -0,0 +1,40 @@
+package com.sham.company.prob1;
+
+import com.google.gson.Gson;
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Combine extends Reducer<Text, Tuple, Text, Tuple> {
+
+    @Override
+    public void reduce(final Text key,
+                       final Iterable<Tuple> values,
+                       final Context context)
+            throws IOException, InterruptedException {
+        HashMap<Text, Float> occupationFrequency = new HashMap<>();
+
+        for (Tuple occupation : values) {
+            Text occupationName = occupation.getOccupation();
+            Float occupationWeight = occupation.getWeight().get();
+
+            Float count = occupationFrequency.getOrDefault(occupationName, 0f);
+
+            occupationFrequency.put(occupationName, count + occupationWeight);
+        }
+
+        for (Map.Entry<Text, Float> entry : occupationFrequency.entrySet()) {
+            Tuple aggregatedOccupations = new Tuple(entry.getKey(), new )
+            context.write(key, new Tuple(entry.getKey(), entry.getValue()));
+        }
+
+
+    }
+}
+
+
+// tuple
